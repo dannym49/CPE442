6b6e8f02f0a9de3bfc99db28ef635dbd4843e1b0
danny49m <dmoren32@calpoly.edu>
2024-12-05 14:31:09 -0800

    FInal Project

--- Final Project/filter.hpp
+++ Final Project/filter.hpp
@@ -5,22 +5,7 @@
 
 using namespace cv;
 
-// // Define RGB to Grayscale coefficients (scaled for integer math)
-// #define R_COEFF 54    // 0.2126 * 256 for integer scaling
-// #define G_COEFF 183   // 0.7152 * 256
-// #define B_COEFF 19    // 0.0722 * 256
 
-// // Sobel filter coefficients
-// extern int verticalFilter[3][3];
-// extern int horizontalFilter[3][3];
-
-// // Structure for thread arguments
-// struct ThreadArgs {
-//     int startRow, endRow, startCol, endCol;
-//     Mat *input;
-//     Mat *grayOutput;
-//     Mat *sobelOutput;
-// };
 
 // Function prototypes
 void neonGrayscale(const Mat& input, Mat& grayOutput, int startRow, int endRow);
@@ -29,5 +14,3 @@ void* applyFilters(void* args);
 void processFrame(Mat &input, Mat &grayOutput, Mat &sobelOutput);
 
 #endif // FILTER_HPP
-
-//g++ main.cpp filter.cpp -o main $(pkg-config --cflags --libs opencv4) -mcpu=cortex-a72 -std=c++11 -O3 -fopenmp
